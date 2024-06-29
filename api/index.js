2063e3a62b1eda10ae89645e45ae12405a56e1a7
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-06-29 13:33:38 +0530

    Connected to data base

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
-mongoose.connect("mongodb+srv://vaporig154:1234@cluster0.azok2y2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+import dotenv from 'dotenv'
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log("Connected to DB")
+})
+.catch((err) => {
+    console.log(err)
+})
 const app = express()
 
 app.listen(3000, () => {
