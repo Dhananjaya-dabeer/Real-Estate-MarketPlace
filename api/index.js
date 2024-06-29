d19dab779a29b8d695bde972321e8d461f50f8ef
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-06-29 13:33:20 +0530

    Connect to data base

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
+mongoose.connect("mongodb+srv://vaporig154:1234@cluster0.azok2y2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const app = express()
 
 app.listen(3000, () => {
