8f0e5d942915dc5fea5c0ea33e750643e6e99721
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-07-14 00:51:57 +0530

    completed header search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 export default function Header() {
 
   const {currentUser} = useSelector(state => state.user)
+  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const URLSearchTerm = urlParams.get('searchTerm')
+    console.log(URLSearchTerm)
+    if(URLSearchTerm){
+      setSearchTerm(URLSearchTerm)
+    }
+  }, [location.search])
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,13 +33,17 @@ export default function Header() {
           <span className='text-slate-700'>Estate</span>
         </h1>
        </Link>
-        <form className='bg-slate-100 flex items-center p-3 rounded-lg'>
+        <form onSubmit={handleSubmit} className='bg-slate-100 flex items-center p-3 rounded-lg'>
           <input
            type="text"
            className='bg-transparent focus:outline-none w-24 sm:w-64' 
            placeholder='Search...'
+           value={searchTerm}
+           onChange={(e) => setSearchTerm(e.target.value)}
            />
+           <button>
            <FaSearch className ='text-slate-600 cursor-pointer'/>
+           </button>
         </form>
         <ul className='flex items-center gap-4 font-medium'>
          <Link to={'/'}> <li className='text-slate-700 hover:underline hidden sm:inline'>Home</li></Link>
